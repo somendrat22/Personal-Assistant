d496e31bae0d23e0ba8f148cb13d51a631ad47a2
Nidhi Kumari <nknidhi321@gmail.com>
2023-06-07 02:28:34 +0530

    Integrated backend with UI

---
+++ routes/Chats/doChat.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const aiModelURL  = 'https://api.openai.com/v1/chat/completions';
+// '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'
+const apiKey = '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';
+const axios  = require('axios');
+let messgaeInput = [];
+
+router.get('/', async (req, res) => {
+    console.log(req.query.text);
+    let mssgObj = {
+        "role" : "user", 
+        "content" : `${req.query.text}`
+    };
+    messgaeInput.push(mssgObj);
+    try{
+        const resposne = await axios.post(aiModelURL, {
+            model : "gpt-3.5-turbo",
+            messages : messgaeInput
+        }, {
+            headers : {
+                'Content-Type' : 'application/json',
+                'Authorization' : `Bearer ${apiKey}`
+            }
+        })
+        //console.log(resposne.data.choices);
+        res.send(resposne.data.choices);
+    }catch(error){
+        //console.log('Error : ', error);
+        res.status(500).send('An Error Occured');
+    }
+    
+}); 
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.use(cors())
 const port = 8001;
 const getModelResponse = require('./routes/ModelConnect/getModelResponse');
 const getChatBotAllChats = require('./routes/ChatBotDBConnect/getAllChats');
+const doChat = require('./routes/Chats/doChat');
+app.use('/api/dochat', doChat);
 app.use('/api/modelconnect', getModelResponse);
 app.use('/api/getallchats', getChatBotAllChats)
 app.listen(port, () => {
